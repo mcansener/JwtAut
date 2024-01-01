082b144fc290a11b61e7d0a8601ca28878910f29
John Sener <john.sener@inspirationtech.co.uk>
2024-01-01 17:55:59 +0000

    first commit

---
+++ Authentication/Controllers/TokenController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Authentication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly string _key;
+
+        public TokenController(IConfiguration configuration)
+        {
+            _issuer = configuration["Jwt:Issuer"]!;
+            _audience = configuration["Jwt:Audience"]!;
+            _key = configuration["Jwt:Key"]!;
+        }
+
+        public string GenerateToken(string userId, string username, string tokenSecretKey)
+        {
+            if (tokenSecretKey != _key)
+            {
+                return "Invalid key provided!";
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_key);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId),
+                new Claim(ClaimTypes.Name, username),
+                // Add more claims as needed
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(1), // Token expiration time
+                Issuer = _issuer,
+                Audience = _audience,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
