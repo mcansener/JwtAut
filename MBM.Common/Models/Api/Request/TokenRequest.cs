5854d8b206355c4af176bec701a5c5c516fcf3ba
John Sener <john.sener@inspirationtech.co.uk>
2024-01-08 15:59:24 +0000

    UI, common, DAL projects added

---
+++ MBM.Common/Models/Api/Request/TokenRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBM.Common.Models.Api.Request
+{
+    public class TokenRequest
+    {
+        public required string UserId { get; set; }
+        public required string Username { get; set; }
+        public required string TokenSecretKey { get; set; }
+    }
+}
---
+++ MBM.DAL.API/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MBM.DAL.API.Models
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Custom configurations can be added here if necessary
+        }
+    }
+}
